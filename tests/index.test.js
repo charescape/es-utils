86e7cf0f73856dba9fdbf6b6c0026f16fcba01e7
charescape <charescape@outlook.com>
2022-03-17 17:19:56 +0800

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,61 @@
 const EsUtils = require('../dist/es-utils.cjs.js');
 
-test('urlsafe_b64', () => {
+test('urlsafe_b64*', () => {
   const url1 = 'https://uname:pwd@test.example.com:9090/path1/path-2?arg=value&arg2=value2#abcd';
   const b641 = 'aHR0cHM6Ly91bmFtZTpwd2RAdGVzdC5leGFtcGxlLmNvbTo5MDkwL3BhdGgxL3BhdGgtMj9hcmc9dmFsdWUmYXJnMj12YWx1ZTIjYWJjZA..';
 
   expect(EsUtils.urlsafe_b64encode(url1)).toBe(b641);
   expect(EsUtils.urlsafe_b64decode(b641)).toBe(url1);
+});
+
+
+const testSearchString = 'q=charCodeAt+charAt&cvid=testcvid&aqs=edge..69i57.test&FORM=ANAB01&PC=U531';
+const testSearchParams = {
+  "q": "charCodeAt charAt",
+  "cvid": "testcvid",
+  "aqs": "edge..69i57.test",
+  "FORM": "ANAB01",
+  "PC": "U531",
+};
+
+test('urlGetSearchString', () => {
+  // expect(EsUtils.urlGetSearchString(test_uri + "?" + testSearchString)).toBe(testSearchString);
+  expect(EsUtils.urlGetSearchString("?" + testSearchString)).toBe(testSearchString);
+  expect(EsUtils.urlGetSearchString(testSearchString)).toBe(testSearchString);
+
+  expect(EsUtils.urlGetSearchString("??" + testSearchString)).toBe(testSearchString);
+  expect(EsUtils.urlGetSearchString("  ??" + testSearchString)).toBe(testSearchString);
+  expect(EsUtils.urlGetSearchString("  " + testSearchString)).toBe(testSearchString);
+
+  expect(EsUtils.urlGetSearchString(testSearchString + " ")).toBe(testSearchString + " ");
+  expect(EsUtils.urlGetSearchString(testSearchString + "?")).toBe(testSearchString + "?");
+  expect(EsUtils.urlGetSearchString(testSearchString + "? ")).toBe(testSearchString + "? ");
+});
+
+test('urlGetSearchParams', () => {
+  // expect(EsUtils.urlGetSearchParams(test_uri + "?" + testSearchString)).toStrictEqual(testSearchParams);
+  expect(EsUtils.urlGetSearchParams("?" + testSearchString)).toStrictEqual(testSearchParams);
+  expect(EsUtils.urlGetSearchParams(testSearchString)).toStrictEqual(testSearchParams);
+
+  expect(EsUtils.urlGetSearchParams("??" + testSearchString)).toStrictEqual(testSearchParams);
+  expect(EsUtils.urlGetSearchParams("  ??" + testSearchString)).toStrictEqual(testSearchParams);
+  expect(EsUtils.urlGetSearchParams("  " + testSearchString)).toStrictEqual(testSearchParams);
+
+  expect(EsUtils.urlGetSearchParams(testSearchString + " ")).toStrictEqual({...testSearchParams, PC: testSearchParams.PC + " "});
+  expect(EsUtils.urlGetSearchParams(testSearchString + "?")).toStrictEqual({...testSearchParams, PC: testSearchParams.PC + "?"});
+  expect(EsUtils.urlGetSearchParams(testSearchString + "? ")).toStrictEqual({...testSearchParams, PC: testSearchParams.PC + "? "});
+});
+
+test('urlGetSearchParam', () => {
+  // expect(EsUtils.urlGetSearchParam("aqs", test_uri + "?" + testSearchString)).toBe(testSearchParams.aqs);
+  expect(EsUtils.urlGetSearchParam("aqs", "?" + testSearchString)).toBe(testSearchParams.aqs);
+  expect(EsUtils.urlGetSearchParam("aqs", testSearchString)).toBe(testSearchParams.aqs);
+
+  expect(EsUtils.urlGetSearchParam("aqs", "??" + testSearchString)).toBe(testSearchParams.aqs);
+  expect(EsUtils.urlGetSearchParam("aqs", "  ??" + testSearchString)).toBe(testSearchParams.aqs);
+  expect(EsUtils.urlGetSearchParam("aqs", "  " + testSearchString)).toBe(testSearchParams.aqs);
+
+  expect(EsUtils.urlGetSearchParam("aqs", testSearchString + " ")).toBe( testSearchParams.aqs);
+  expect(EsUtils.urlGetSearchParam("aqs", testSearchString + "?")).toBe(testSearchParams.aqs);
+  expect(EsUtils.urlGetSearchParam("aqs", testSearchString + "? ")).toBe(testSearchParams.aqs);
 });
