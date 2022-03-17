2bc15708171b21ac8345185bcab3c3bf39958d57
charescape <charescape@outlook.com>
2022-03-17 17:19:58 +0800

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,6 @@
+import { ParsedQs } from "qs";
 export declare function urlsafe_b64encode(value: string): string;
 export declare function urlsafe_b64decode(value: string): string;
+export declare function urlGetSearchString(wls?: string): string;
+export declare function urlGetSearchParams(wls?: string): ParsedQs;
+export declare function urlGetSearchParam(param: string, wls?: string): any;
