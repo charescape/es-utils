94d985a53a85c4c1826729218134d9c8e2c37742
charescape <charescape@outlook.com>
2022-05-06 01:49:43 +0800

    update

---
+++ src/url.ts
@@ -0,0 +1,57 @@
+import {encode, decode} from "js-base64";
+import {isNil, isString} from "./is";
+import {parse, ParsedQs} from "qs";
+
+export function urlSafeBase64Encode(value: string): string {
+  return encode(value)
+    .replaceAll('+', '-')
+    .replaceAll('/', '_')
+    .replaceAll('=', '.')
+    ;
+}
+
+export function urlSafeBase64Decode(value: string): string {
+  return decode(
+    value
+      .replaceAll('.', '=')
+      .replaceAll('_', '/')
+      .replaceAll('-', '+')
+  );
+}
+
+export function urlGetSearchString(wls?: string): string {
+  if (!isString(wls)) {
+    wls = window.location.search;
+  }
+
+  wls = (wls as string).trimStart();
+
+  if ((wls as string).startsWith('http:') || (wls as string).startsWith('https:')) {
+    const arr = (wls as string).split('?');
+    if (arr.length > 1) {
+      delete arr[0];
+    }
+
+    wls = arr.join('');
+  } else {
+    if (wls.charAt(0) === '?') {
+      wls = wls.replace(/^[?]+/, '');
+    }
+  }
+
+  return `${wls}`;
+}
+
+export function urlGetSearchParams(wls?: string): ParsedQs {
+  return parse(urlGetSearchString(wls));
+}
+
+export function urlGetSearchParam(param: string, wls?: string): any {
+  const params = urlGetSearchParams(wls);
+
+  if (!isNil(params[param])) {
+    return params[param];
+  }
+
+  return null;
+}
